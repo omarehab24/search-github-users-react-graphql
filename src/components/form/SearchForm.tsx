0c90758e38740f2b5bf23e34f420240bf0b05c1f
Omar Ehab <89408282+omarehab24@users.noreply.github.com>
2025-02-23 12:17:28 +0200

    Setup SearchForm component and Use Toaster component in main.tsx to use with other components

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,11 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { type FormEvent } from 'react';
+import { useState } from 'react';
+import { toast } from "sonner"
+
 type SearchFormProps = {
     userName: string
     setUserName: React.Dispatch<React.SetStateAction<string>>
 }
 
 const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
+    const [text, setText] = useState(userName)
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (text === '') {
+            toast('Please enter a name')
+            return;
+        }
+        setUserName(text)
+    }
     return (
-        <div>SearchForm</div>
+        <form onSubmit={handleSearch} className='flex items-center gap-x-2 w-full lg:w-1/3 mb-8'>
+            <Label htmlFor='name' className='sr-only'>Name</Label>
+            <Input id='name' placeholder='Search for a user' value={text} onChange={(e) => setText(e.target.value)} className='flex-grow bg-background' />
+            <Button type='submit'>Search</Button>
+        </form>
     )
 }
 export default SearchForm
