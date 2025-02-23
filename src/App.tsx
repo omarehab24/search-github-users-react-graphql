ae5cd43853e53633adf8706ae963e5043c7d8d50
Omar Ehab <89408282+omarehab24@users.noreply.github.com>
2025-02-23 11:13:26 +0200

    Initial commit in branch dev

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex gap-4">
+        <Button>Click me</Button>
+        <Button variant='outline' size='lg'>
+          Click me
+        </Button>
+        <Button variant='destructive' size='sm'>
+          Click me
+        </Button>
+      </div>
+    </div>
+  )
+};
+export default App;
