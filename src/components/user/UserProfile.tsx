d625d19c7817b4ac3238b06ddb33e6d27061f843
Omar Ehab <89408282+omarehab24@users.noreply.github.com>
2025-02-23 17:29:24 +0200

    Setup GraphQL queries, types and Setup UserProfile with data, loading & error states from ApolloClient's useQuery

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,12 +1,26 @@
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '@/queries';
+import { UserData } from '@/types';
+
 type UserProfileProps = {
     userName: string;
 }
 
 const UserProfile = ({ userName }: UserProfileProps) => {
+    const { loading, error, data } = useQuery<UserData>(GET_USER, {
+        variables: { login: userName },
+    });
+
+    if (loading) return <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Loading...</h2>
+    if (error) return <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Error: {error.message}</h2>
+    if (!data) return <h2 className="text-2xl font-bold text-gray-900 dark:text-white">User not found</h2>
+
+    const { name, bio, avatarUrl, url, repositories, followers, following, gists } = data.user
+
     return (
-        <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
-            {userName}
-        </h2>
+        <div>
+            <h1>{bio}</h1>
+        </div>
     )
 }
 export default UserProfile
